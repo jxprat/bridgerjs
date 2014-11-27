e18ca0249a0dd9b4063d7607d2b706048fd37bbf
Jordi Xavier Prat <jxprat@gmail.com>
2014-11-27 16:42:10 +0100

    commits iniciales ...

--- bridger.js
+++ bridger.js
@@ -1,1 +1,32 @@
- 
+//-------------------------------------------------------
+// Bridger!
+//-------------------------------------------------------
+
+var aSuits = ["S", "H", "D", "C"];		// Spades, Hearts, Diamonds and Clubs
+var aRank = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"];
+
+/*
+var oTest = {
+	nom: '',
+	edat: 36,
+	met_A: function(a,b)
+	{
+		return a+b;
+	}
+	,
+	met_b: function()
+	{
+		console.log("Hola");
+	}
+}
+
+function diego()
+{
+	return 67;
+}
+
+oTest.met_C = diego;
+
+
+oTest.met_C();
+*/
